7b3f7b47e329e5f0f14a54c44e6a2b7a83a230c8
natsumitahira <na-tsu-mi_12t.14@ymail.ne.jp>
2024-11-24 19:51:26 +0900

    JSanimation_re

--- accordion-anime/work/js/app.js
+++ accordion-anime/work/js/app.js
@@ -1,18 +1,13 @@
 document.querySelectorAll('.list-tit').forEach(title => {
-    title.addEventListener('click', () => {
-      const content = title.nextElementSibling;
-        if (content.style.height && content.style.height !== '0px') {
-        content.style.height = '0';
-        content.style.padding = '0';
-      } else {
-        document.querySelectorAll('.list-content').forEach(otherContent => {
-          otherContent.style.height = '0';
-          otherContent.style.padding = '0';
-        });
-        const contentHeight = content.scrollHeight; // 要素の本来の高さを取得
-        content.style.height = contentHeight + 'px';
-        content.style.padding = '1rem';
-      }
-    });
+  title.addEventListener('click', () => {
+    const content = title.nextElementSibling;
+    if (content.style.height && content.style.height !== '0px') {
+      content.style.height = '0';
+      content.style.padding = '0';
+    } else {
+      const contentHeight = content.scrollHeight;
+      content.style.height = contentHeight + 'px';
+      content.style.padding = '1rem';
+    }
   });
-  
+});
